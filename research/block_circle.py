e85bbebaa6d80f48750c84e60603f24bf898e61d
d-002 <minecraftenjoy@outlook.fr>
2025-06-23 13:48:21 +0200

    add multiple blocks in block_circle

--- research/block_circle.py
+++ research/block_circle.py
@@ -1,15 +1,14 @@
 import pygame
-from pygame.locals import Rect, KEYDOWN, MOUSEWHEEL, K_ESCAPE, QUIT
+from pygame.locals import Rect, K_ESCAPE, QUIT, KEYDOWN, MOUSEWHEEL, MOUSEBUTTONDOWN
 from pygame.math import Vector2 as v2
 
 from math import cos, sin, atan2
 
-_range = 1
-angle = .5
-
 pygame.init()
 
-RADIUS = 120
+displacement = .3
+
+RADIUS = 100
 W, H = 640, 480
 center = v2(W/2, H/2)
 screen = pygame.display.set_mode((W, H))
@@ -20,6 +19,37 @@ def point(pos, color=(0, 0, 0), radius=7):
     pygame.draw.circle(screen, color, pos, radius)
 
 
+class Block:
+    def __init__(self):
+        self.range = .5
+        self.angle = 0
+        self.radius = RADIUS * (1+displacement)
+
+    def update(self, events):
+        # change range
+        for event in events:
+            if event.type == MOUSEWHEEL:
+                self.range += .05 * event.y
+
+        # change rotation
+        diff = pygame.mouse.get_pos()-center
+        self.angle = atan2(diff.y, diff.x)
+
+    def display(self):
+        # display range
+        a0, a1 = self.angle-self.range, self.angle+self.range
+        A = center + self.radius * v2(cos(a0), sin(a0))
+        B = center + self.radius * v2(cos(a1), sin(a1))
+        point(A)
+        point(B)
+
+        # inverted y, need to sway the angles
+        v = v2(self.radius, self.radius)
+        pygame.draw.arc(screen, (0, 0, 0), Rect(center-v, v*2), -a1, -a0)
+
+
+blocks = [Block()]
+
 run = True
 while run:
     events = pygame.event.get()
@@ -28,27 +58,19 @@ while run:
                 event.type == KEYDOWN and event.key == K_ESCAPE:
             run = False
 
-        # change range
-        if event.type == MOUSEWHEEL:
-            _range += .05 * event.y
+        if event.type == MOUSEBUTTONDOWN:
+            # make the last block static, and add a new one a bit further away
+            displacement += .1
+            blocks.append(Block())
 
-    # change rotation
-    diff = pygame.mouse.get_pos()-center
-    angle = atan2(diff.y, diff.x)
-
+    # display circle
     screen.fill((127, 255, 127))
     pygame.draw.circle(screen, (63, 191, 63), center, RADIUS)
     pygame.draw.circle(screen, (0, 0, 0), center, RADIUS, width=1)
 
-    # display range
-    angle0, angle1 = angle-_range, angle+_range
-    A = center + RADIUS*1.5 * v2(cos(angle0), sin(angle0))
-    B = center + RADIUS*1.5 * v2(cos(angle1), sin(angle1))
-    point(A)
-    point(B)
-    # inverted y, need to sway the angles
-    v = v2(RADIUS*1.5, RADIUS*1.5)
-    pygame.draw.arc(screen, (0, 0, 0), Rect(center-v, v*2), -angle1, -angle0)
+    blocks[-1].update(events)
+    for block in blocks:
+        block.display()
 
     pygame.display.flip()
     clock.tick(60)
